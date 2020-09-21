2bfc90b53ffb9e7231901aa3c46429e41b5436f8
Inez Escandon <iescan4@gmail.com>
2020-09-20 22:46:22 -0500

    added lead p tag in jumbotron to add home link and contact link

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -9,6 +9,10 @@ function Jumbotron({ search, handleInputChange, getLatAndLong, panTo }) {
 				<div className="row">
 					<div className="col">
 						<h1 className="display-4">Are the trails open?</h1>
+						{/* <p class="lead">
+							This is a modified jumbotron that occupies the entire horizontal
+							space of its parent.
+						</p> */}
 					</div>
 					<div className="col mt-4">
 						<Search

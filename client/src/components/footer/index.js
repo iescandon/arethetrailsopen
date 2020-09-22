233693e041d6fd16e4699fbcb24ee68242e871d9
Inez Escandon <iescan4@gmail.com>
2020-09-22 15:24:01 -0500

    broke my app nooooo

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Container } from 'react-bootstrap';
 import './style.css';
 
 function Footer() {
 	return (
-		// <div className="fixed-bottom">
-		// 	<Navbar color="dark" dark>
-		// 		<Container>
-		// 			<NavbarBrand>© Copyright 2020</NavbarBrand>
-		// 		</Container>
-		// 	</Navbar>
-		// </div>
-		<div className="container footer">
-			<div className="text-center py-3">© Copyright 2020</div>
-		</div>
+		<footer>
+			<div className="container footer">
+				<div className="text-center pb-3 pt-3">© Copyright 2020</div>
+			</div>
+		</footer>
 	);
 }
 

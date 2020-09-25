ff46d1109612499287f1bc44b84374d43039dd9f
Inez Escandon <iescan4@gmail.com>
2020-09-25 09:02:42 -0500

    added pull to refresh package

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,22 @@ import AboutPage from './pages/aboutpage';
 import HelpPage from './pages/helppage';
 import ContactPage from './pages/contactpage';
 import Footer from './components/footer';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 
 function App() {
+	const handleRefresh = () => {
+		window.location.reload(false);
+	};
 	return (
 		<Router>
 			<div>
-				<Route exact path="/" component={HomePage} />
-				<Route exact path="/home" component={HomePage} />
-				<Route exact path="/about" component={AboutPage} />
-				<Route exact path="/help" component={HelpPage} />
-				<Route exact path="/contact" component={ContactPage} />
+				<PullToRefresh onRefresh={handleRefresh}>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/home" component={HomePage} />
+					<Route exact path="/about" component={AboutPage} />
+					<Route exact path="/help" component={HelpPage} />
+					<Route exact path="/contact" component={ContactPage} />
+				</PullToRefresh>
 			</div>
 			<Footer path="/" />
 		</Router>

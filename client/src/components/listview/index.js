32fce993888a6cabef26b3b66ae8e8e0203551cd
Inez Escandon <iescan4@gmail.com>
2020-10-28 09:11:23 -0500

    got sort to work yayyy thanks brandon

--- client/src/components/listview/index.js
+++ client/src/components/listview/index.js
@@ -4,66 +4,73 @@ import { getDistance } from 'geolib';
 import './style.css';
 
 function ListView({ trails, updateCurrentDate, selectTrail, centerPoint }) {
+	let trailsArray = trails;
+
+	trailsArray.map((trail) => {
+		trail.distance = (
+			getDistance(
+				{ latitude: trail.lat, longitude: trail.lng },
+				{
+					latitude: centerPoint.lat,
+					longitude: centerPoint.lng,
+				}
+			) * 0.000621371192
+		).toFixed(2);
+	});
+
+	trailsArray.sort(function (a, b) {
+		return a.distance - b.distance;
+	});
+
 	return (
 		<Table hover className="table list-view">
 			<tbody>
-				{trails
-					.map((trail) => {
-						let status = '';
-						if (trail.open === 'true') {
-							status = 'OPEN';
-						}
-						if (trail.open === 'false') {
-							status = 'CLOSED';
-						}
-						if (trail.open === 'unknown') {
-							status = 'UNKNOWN';
-						}
-						return (
-							<tr key={trail.trailId}>
-								<td
-									className="trailRow"
-									onClick={() => {
-										selectTrail(trail);
-									}}
-								>
-									<div className="row">
-										<div className="col-11">
-											<div className="row pl-4" id="listrow">
-												<div>
-													<span className={`trailStatus ${trail.open}`}>
-														{status}
-													</span>
-													<span className="pl-3 trailUpdated">
-														{updateCurrentDate(trail.lastToggled)}
-													</span>
-												</div>
-											</div>
-											<div className="row pl-4 trailName" id="listrow">
-												{trail.name}
-											</div>
-											<div className="row pl-4 trailDistance" id="listrow">
-												{(
-													getDistance(
-														{ latitude: trail.lat, longitude: trail.lng },
-														{
-															latitude: centerPoint.lat,
-															longitude: centerPoint.lng,
-														}
-													) * 0.000621371192
-												).toFixed(2)}{' '}
-												mi away
+				{trailsArray.map((trail) => {
+					let status = '';
+					if (trail.open === 'true') {
+						status = 'OPEN';
+					}
+					if (trail.open === 'false') {
+						status = 'CLOSED';
+					}
+					if (trail.open === 'unknown') {
+						status = 'UNKNOWN';
+					}
+					return (
+						<tr key={trail.trailId}>
+							<td
+								className="trailRow"
+								onClick={() => {
+									selectTrail(trail);
+								}}
+							>
+								<div className="row">
+									<div className="col-11">
+										<div className="row pl-4" id="listrow">
+											<div>
+												<span className={`trailStatus ${trail.open}`}>
+													{status}
+												</span>
+												<span className="pl-3 trailUpdated">
+													{updateCurrentDate(trail.lastToggled)}
+												</span>
 											</div>
 										</div>
-										<div className="col-1 my-auto">
-											<i className="fa fa-angle-right" aria-hidden="true"></i>
+										<div className="row pl-4 trailName" id="listrow">
+											{trail.name}
+										</div>
+										<div className="row pl-4 trailDistance" id="listrow">
+											{trail.distance} mi away
 										</div>
 									</div>
-								</td>
-							</tr>
-						);
-					})
-					.sort()}
+									<div className="col-1 my-auto">
+										<i className="fa fa-angle-right" aria-hidden="true"></i>
+									</div>
+								</div>
+							</td>
+						</tr>
+					);
+				})}
 			</tbody>
 		</Table>
 	);
